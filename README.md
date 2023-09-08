f04d285d015a1dfc841bf69da579820b9886fb79
Dprof-Code <olawaleade15@gmail.com>
2023-09-08 02:53:17 -0500

    completed task

---
+++ README.md
@@ -0,0 +1,7 @@
+# HNGx STAGE ONE TASK
+
+## Task: Frontend Page Creation with Specific Elements
+
+### Objective: Develop and host a web page using HTML, CSS and JS that showcases specific personal and real-time data attributes. Each required element should have a specified data-testid attribute for easy identification and testing.
+
+This is the first task in the HNGx Internship program. The task is to build a web page that contains username, user profile picture, github repo link, day and time.
---
+++ script.js
@@ -0,0 +1,46 @@
+let timeWrap = document.getElementById("time");
+let day = document.getElementById("day");
+
+
+
+
+function time() {
+    let date = new Date();
+    let hour = date.getUTCHours();
+    let mins = date.getUTCMinutes();
+    let secs = date.getUTCSeconds();
+
+    hour < 10 ? hour = "0" + hour : hour = hour;
+
+    mins < 10 ? mins = "0" + mins : mins = mins;
+
+    secs < 10 ? secs = "0" + secs : secs = secs;
+
+    timeWrap.innerText = hour + " : " + mins + " : " + secs;
+
+    switch (date.getDay()) {
+        case 0:
+            day.innerText = "Sunday";
+            break;
+        case 1:
+            day.innerText = "Monday";
+            break;
+        case 2:
+            day.innerText = "Tuesday";
+            break;
+        case 3:
+            day.innerText = "Wednesday";
+            break;
+        case 4:
+            day.innerText = "Thursday";
+            break;
+        case 5:
+            day.innerText = "Friday";
+            break;
+        case 6:
+            day.innerText = "Saturday";
+            break;
+    }
+}
+
+setInterval(time, 1000);
